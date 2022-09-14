325ba27defaae4b40de98c2fb029a79c36171ae1
Leandro Elias Goerck <lgoerck@universo.univates.br>
2022-09-14 20:28:47 -0300

    feat(file_management): add file management txt importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        return print("Formato inválido", file=sys.stderr)
+
+    try:
+        with open(path_file) as file:
+            arquivo_txt = file.read().splitlines() 
+            return arquivo_txt
+        
+    except:
+        print("Arquivo " + path_file + " não encontrado", file=sys.stderr)
+
