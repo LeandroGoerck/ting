c44f5117b54f4953b521ba40031b38ddfd945785
Leandro Elias Goerck <lgoerck@universo.univates.br>
2022-09-14 19:26:35 -0300

    style(queue): code formatting

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,19 +1,17 @@
 class Queue:
     def __init__(self):
-        self.data = list();
+        self.data = list()
 
     def __len__(self):
-        return len(self.data);
+        return len(self.data)
 
     def enqueue(self, value):
-        self.data.append(value);
+        self.data.append(value)
 
     def dequeue(self):
-        return self.data.pop(0);
-
+        return self.data.pop(0)
 
     def search(self, index):
         if (index < 0 or index >= len(self.data)):
-            raise IndexError("Index out of range");
-        
-        return self.data[index];
+            raise IndexError("Index out of range")
+        return self.data[index]
