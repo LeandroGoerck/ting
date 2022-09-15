1836a23efe67827c60e7360630a59b869593cf4f
Leandro Elias Goerck <lgoerck@universo.univates.br>
2022-09-14 23:24:43 -0300

    feat(file_process): add method file_process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,26 @@
+import sys
+from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if isinstance(instance, Queue):
+        queue = instance.get_queue()
+
+        for element in queue:
+            if element["nome_do_arquivo"] == path_file:
+                return None
+
+        file = txt_importer(path_file)
+
+        new_element = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(file),
+            "linhas_do_arquivo": file,
+        }
+
+        instance.enqueue(new_element)
+        print(new_element, file=sys.stdout)
 
 
 def remove(instance):
