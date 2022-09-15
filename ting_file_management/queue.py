b132a03022215e57e524187ddca6ad18d404c1ee
Leandro Elias Goerck <lgoerck@universo.univates.br>
2022-09-14 23:24:01 -0300

    feat(queue): add method to return the data

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -12,6 +12,9 @@ class Queue:
         return self.data.pop(0)
 
     def search(self, index):
-        if (index < 0 or index >= len(self.data)):
+        if index < 0 or index >= len(self.data):
             raise IndexError("Index out of range")
         return self.data[index]
+
+    def get_queue(self):
+        return self.data
