51d4a8d809d1d28672ecff4f990ad5644baf10ee
Leandro Elias Goerck <lgoerck@universo.univates.br>
2022-09-15 23:44:43 -0300

    feat(word_search): add exists_word
    exists_word verifies if a given word exists within an file previously organized as a queue

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,34 @@
+from ting_file_management.queue import Queue
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    print(type(instance))
+    if isinstance(instance, Queue):
+        result = []
+        queue = instance.get_queue()
+
+        for element in queue:
+            a_word_was_found = False
+            ocorrencias = []
+            file_lines = element["linhas_do_arquivo"]
+            for index, line in enumerate(file_lines):
+                line_in_lower_case = line.lower()
+                word_in_lower_case = word.lower()
+                if word_in_lower_case in line_in_lower_case:
+                    ocorrencias.append({"linha": (index + 1)})
+                    a_word_was_found = True
+            if a_word_was_found == True:
+                result.append(
+                    {
+                        "palavra": word,
+                        "arquivo": element["nome_do_arquivo"],
+                        "ocorrencias": ocorrencias,
+                    }
+                )
+
+        return result
+    else:
+        return None
 
 
 def search_by_word(word, instance):
