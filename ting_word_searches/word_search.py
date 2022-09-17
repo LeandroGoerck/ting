df2e8033d6e5796cb33f74e3ff18036bbbe486f2
Leandro Elias Goerck <lgoerck@universo.univates.br>
2022-09-16 23:13:30 -0300

    feat(word_search): add search by word in processed files

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -25,4 +25,26 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    queue = instance.get_queue()
+
+    for element in queue:
+        a_word_was_found = False
+        ocorrencias = []
+        file_lines = element["linhas_do_arquivo"]
+        for index, line in enumerate(file_lines):
+            line_in_lower_case = line.lower()
+            word_in_lower_case = word.lower()
+            if word_in_lower_case in line_in_lower_case:
+                ocorrencias.append({"linha": (index + 1), "conteudo": line})
+                a_word_was_found = True
+        if a_word_was_found:
+            result.append(
+                {
+                    "palavra": word,
+                    "arquivo": element["nome_do_arquivo"],
+                    "ocorrencias": ocorrencias,
+                }
+            )
+
+    return result
